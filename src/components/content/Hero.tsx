affd3ce52d9d9ae7ede222d55ed7c76e2feda596
FarciarzYT <140611697+FarciarzYT@users.noreply.github.com>
2026-04-23 22:41:52 +0200

    add padding to align img

--- src/components/content/Hero.tsx
+++ src/components/content/Hero.tsx
@@ -6,7 +6,7 @@ import IconsBar from "@/components/content/IconsBar";
 export default function Hero() {
     return (
 
-        <section className="hero max-w-3xl w-max-sm sm:max-w-xl gap-6 mx-auto justify-center items-center sm:items-start mt-32 sm:mt-48 flex flex-col-reverse sm:flex-row ">
+        <section className="hero max-w-3xl w-max-sm sm:max-w-xl gap-6 mx-auto justify-center items-center sm:items-start mt-32 flex flex-col-reverse sm:flex-row ">
             <div className={"w-[50%] max-w-[50%] flex flex-col"}>
                 <h1 className="text-3xl font-bold tracking-tight text-black">
                     Tomasz Tłusty
@@ -16,11 +16,11 @@ export default function Hero() {
                     Web Developer | Tarnobrzeg 🇵🇱
                 </h2>
 
-                <p className="mt-4 font-medium leading-relaxed text-black max-w-xl">
+                <p className="mt-4 font-medium leading-relaxed text-black sm:w-full max-w-xl">
                     Projektuję. Koduję. Wdrażam.
                 </p>
 
-                <div className="flex flex-row mt-4 items-center gap-2">
+                <div className="flex flex-row mt-7.5 items-center gap-2">
                     <Link
                         href={"/cv"}
                         className="flex items-center text-sm gap-2 font-semibold  hover:text-black w-fit bg-mintcream/80 border-2 border-black text-black/80 rounded-3xl py-0.5 px-3 hover:translate-x-1 hover:-translate-y-0.5 transition-all duration-300 backdrop-blur-sm"
@@ -32,8 +32,14 @@ export default function Hero() {
                         <IconsBar/>
                     </div>
                 </div>
+                <Link
+                    href={"/cv"}
+                    className="flex items-center text-sm gap-2 font-semibold mt-2 hover:text-black w-fit bg-mintcream/80 border-2 border-black text-black/80 rounded-3xl py-0.5 px-3 hover:translate-x-1 hover:-translate-y-0.5 transition-all duration-300 backdrop-blur-sm"
+                >
+                    <span>Napisz do mnie </span>
+                    <SlDocs className="text-xs transition-all duration-150" />
+                </Link>
             </div>
-
             <div className={"w-[50%] max-w-[50%]"}>
                 <Image src={"/tlusty.png"} alt={"Selfie Tomasza"} width={400} height={250} className={"rounded-3xl"} />
             </div>
